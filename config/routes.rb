a2f7df1ec9ac4916d5be31c29ed1f55bfd46038e
mzane42 <mzane@student.42.fr>
2017-06-17 07:28:31 +0200

    edit ReadMe

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get '/battles/battle/:id/hit', to: 'battles#hit'
   get '/battles/win/:id', to: 'battles#win'
   get '/battles/loose', to: 'battles#loose'
+  get '/clear', to: 'application#clear'
 
 end
